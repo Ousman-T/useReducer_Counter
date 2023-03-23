e0785097d1f18ea7f82fbb7423bd6ea538076e01
Ousman Touray <otouray14@gmail.com>
2023-03-23 13:51:39 -0400

    created a counter utilizing new hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { createContext, useReducer } from 'react';
+import HomePage from './pages/HomePage';
 import './App.css';
 
+// initializing our context
+export const MyContext = createContext(null);
+
 function App() {
+  // initialize our useReducer
+  const initialState = 0;
+  // pass a reducer function and the initial state to the useReducer
+  const [state, dispatch] = useReducer(reducer, initialState)
+  // reducer function which will have all the logic to update the state
+  function reducer(state, action){
+    const {type, payload} = action;
+    console.log(action);
+    switch(action.type){
+      case 'add':
+        return state + 1;
+      case 'subtract':
+        return state - 1;
+      case 'add_five':
+        return state + payload;
+      default:
+          return state;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyContext.Provider value={{state, dispatch}} >
+      <div className="App">
+      <HomePage />
+      </div>
+    </MyContext.Provider>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import Counter from "../components/Counter";
+
+function HomePage (){
+    return(
+            <div>
+            <h1>HomePage</h1>
+            <Counter />
+            </div>  
+        )
+}
+
+export default HomePage;
